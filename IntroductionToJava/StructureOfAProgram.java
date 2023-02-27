61fc1bbe4f320b776065f77f44273d69d0b84421
Rushan Shaikh <91390701+rush4n@users.noreply.github.com>
2023-02-27 16:40:40 +0530

    Add files via upload

---
+++ IntroductionToJava/StructureOfAProgram.java
@@ -0,0 +1,7 @@
+package IntroductionToJava;
+public class StructureOfAProgram {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("Hello world");
+    }
+}
---
+++ IntroductionToJava/VariablesAndDataTypes.java
@@ -0,0 +1,13 @@
+package IntroductionToJava;
+public class VariablesAndDataTypes {
+            public static void main(String[] args) {
+                System.out.println("The sum of these numbers is : ");
+                int num1 = 10;
+                int num2 = 15;
+                int num3 = 30;
+                int sum = num1 + num2 + num3;
+                System.out.println(sum);
+
+            }
+    }
+
