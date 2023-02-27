7c6cfbe8545b04949208711d572cdcbc013b9251
Rushan Shaikh <91390701+rush4n@users.noreply.github.com>
2023-02-27 21:29:33 +0530

    Add files via upload

---
+++ IntroductionToJava/Literals.java
@@ -0,0 +1,18 @@
+package IntroductionToJava;
+
+public class Literals {
+    public static void main(String[] args){
+        byte age = 18;
+        short c = 8;
+        int d = 20;
+        char name = 'R';
+        long b = 526356263l;
+        float value = 3.02f;
+        double val = 6.66;
+        boolean a = true;
+        String name1 = "Rushan";
+        // Constant provided value is literal
+
+
+    }
+}
