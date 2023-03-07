c2882fbe2094b33c827f76ed4c2aeed395226988
Rushan Shaikh <91390701+rush4n@users.noreply.github.com>
2023-03-07 11:49:05 +0530

    Add files via upload

---
+++ Strings/Introduction.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Introduction {
+    public static void main(String[] args){
+        String name = new String("Rushan Shaikh");
+        System.out.println(name);
+        String name2 = "Rushan";
+        System.out.println(name2);
+        int a = 7;
+        float b = 13.37f;
+        System.out.printf("The value of a is %d and value of b is %.2f\n", a, b);
+        System.out.format("The value of a is %d and value of b is %.2f\n", a, b);
+
+        Scanner in = new Scanner(System.in);
+        String S = in.nextLine();
+        System.out.printf("The input value of Sting is : %s ", S);
+
+
+    }
+}
