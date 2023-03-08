a62c5f04d021de267606c31bd0fac244138c1ed9
Rushan Shaikh <91390701+rush4n@users.noreply.github.com>
2023-03-08 13:26:10 +0530

    Update WhileLoop.java

--- ConditionalLoops/WhileLoop.java
+++ ConditionalLoops/WhileLoop.java
@@ -4,7 +4,7 @@ public class WhileLoop {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int i = in.nextInt();
-        while (i<=10){
+        while (i<=100){
             System.out.println(i);
             i++;
         }
